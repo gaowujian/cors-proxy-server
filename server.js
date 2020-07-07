8391e0fdb55443843aa9ffb5aed6cd055301198e
Tony Gao <806003097@qq.com>
2020-07-07 09:10:33 +0800

    update

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(cors());
+app.use("*", (req, res, next) => {
+  res.setHeader("Set-Cookie", JSON.stringify({ id: "110ewer" }));
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/api/user", (req, res, next) => {
+  //   res.setHeader("Access-Control-Allow-Origin", "*");
+  const user = {
+    user: "tony",
+    age: 28,
+  };
+  res.json(user);
+});
 
 app.get("/api/user", (req, res) => {
   //   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,6 +33,6 @@ app.get("/api/user", (req, res) => {
 app.get("/b", (req, res) => {
   res.send("b");
 });
-app.listen(5000, () => {
-  console.log("listening 5000");
+app.listen(5050, () => {
+  console.log("listening 5050");
 });
